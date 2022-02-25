4e8bb413bd83d9924369acb2b41b18dad397e132
MahbubTonoy <tnymrbadboy@gmail.com>
2022-02-25 22:51:39 +0600

    take a php class.

--- OOP_Class/index.php
+++ OOP_Class/index.php
@@ -1,13 +1,4 @@
 <?php
 
-// require "user.php";
-
-// $user1 = new User("gobinda", "gobinda@gmail.com");
-
-// echo $user1->getEmail();
-// echo "<br/>";
-// $user1->setEmail("gobinda2@gmail.com");
-// echo $user1->getEmail();
-// echo "<br/>";
-
-// echo $_SERVER['HTTP_REFERER'];
+$x = "<script>alert('hello');</script>";
+echo htmlspecialchars($x);
