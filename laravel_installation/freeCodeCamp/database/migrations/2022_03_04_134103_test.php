7426b6298527bb0afb21218c225f05f39c5fc35f
MahbubTonoy <tnymrbadboy@gmail.com>
2022-03-04 22:31:51 +0600

    I don't remember how many things done today. sorry.

--- laravel_installation/freeCodeCamp/database/migrations/2022_03_04_134103_test.php
+++ laravel_installation/freeCodeCamp/database/migrations/2022_03_04_134103_test.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create("name", function(Blueprint $table){
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string("First_Name");
+            $table->string("Last_Name");
+            $table->string("email")->nullable();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        //
     }
 };
--- laravel_installation/freeCodeCamp/routes/web.php
+++ laravel_installation/freeCodeCamp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Handler;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('a');
+
+
+// Route::get("/tonoy", [
+//     "uses"=> "App\Http\Controllers\Handler@Test",
+//     "as"=> "test"
+// ]);
+
+
+Route::get('/tonoy',[Handler::class,'test'])->name('test');
