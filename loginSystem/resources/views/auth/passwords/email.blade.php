e1f122f5a5e01e6e23c8651b2b6c19c0a5b0e699
MahbubTonoy <tnymrbadboy@gmail.com>
2022-03-18 00:00:56 +0600

    completed the login and registration system. need more thing to do.

--- loginSystem/resources/views/auth/passwords/email.blade.php
+++ loginSystem/resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,42 @@
-@extends('layouts.app')
+@extends('auth.authMaster')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@section('section')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+  <div class="main">
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+    <!-- Verify  Form -->
+    <section class="sign-in">
+      <div class="container">
+        <div class="signin-content">
+          <div class="signin-image">
+            <figure><img src="{{asset('images/signin-image.jpg')}}" alt="sign up image"></figure>
+            <a href="{{route('register')}}" class="signup-image-link">Create an account</a>
+          </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+          <div class="signin-form">
+            <h2 class="form-title">Password Recovery</h2>
+            <form method="POST" action="{{ route('password.email') }}" class="register-form" id="login-form">
+              @csrf
+              <div class="form-group">
+                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                <input type="text" cclass="@error('email') is-invalid @enderror" name="email" id="your_name" placeholder="Email" />
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+              </div>
+              <div class="form-group form-button">
+                <input type="submit" name="signin" id="signin" class="form-submit" value="{{ __('Send Password Reset Link') }}" />
+              </div>
+            </form>
+            <div class="social-login">
+              <a href="{{route('login')}}" class="social-label">Have an Account?</a>
             </div>
+          </div>
         </div>
-    </div>
-</div>
-@endsection
+      </div>
+    </section>
+
+  </div>
+  @endSection
--- loginSystem/resources/views/auth/register.blade.php
+++ loginSystem/resources/views/auth/register.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('auth.authMaster')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Sign Up Form by Colorlib</title>
-
-  <!-- Font Icon -->
-  <link rel="stylesheet" href="{{asset('frontEnd/fonts/material-icon/css/material-design-iconic-font.min.css')}}">
-
-  <!-- Main css -->
-  <link rel="stylesheet" href="{{asset('frontEnd/css/app.css')}}">
-</head>
-
-<body>
+@section('section')
 
   <div class="main">
 
@@ -24,22 +10,38 @@
         <div class="signup-content">
           <div class="signup-form">
             <h2 class="form-title">Sign up</h2>
-            <form method="POST" class="register-form" id="register-form">
+            <form method="POST" action="{{ route('register') }}" class="register-form" id="register-form">
+              @csrf
               <div class="form-group">
                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                <input type="text" name="name" id="name" placeholder="Your Name" />
+                <input type="text" class="@error('name') is-invalid @enderror" name="name" id="name" placeholder="Your Name" />
+                @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
               </div>
               <div class="form-group">
                 <label for="email"><i class="zmdi zmdi-email"></i></label>
-                <input type="email" name="email" id="email" placeholder="Your Email" />
+                <input type="email" class="@error('email') is-invalid @enderror" name="email" id="email" placeholder="Your Email" />
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
               </div>
               <div class="form-group">
                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                <input type="password" name="pass" id="pass" placeholder="Password" />
+                <input type="password" class="@error('password') is-invalid @enderror" name="password" id="pass" placeholder="Password" />
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
               </div>
               <div class="form-group">
                 <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" />
+                <input type="password" name="password_confirmation" id="re_pass" placeholder="Repeat your password" />
               </div>
               <div class="form-group">
                 <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
@@ -59,10 +61,4 @@
     </section>
 
   </div>
-
-  <!-- JS -->
-  {{-- <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="js/main.js"></script> --}}
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
-
-</html>
+  @endSection
